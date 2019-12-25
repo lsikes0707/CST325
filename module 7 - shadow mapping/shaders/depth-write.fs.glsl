5d728badfdc8d431cc3a9ae795e2e6a160fcabf5
Lacey Sikes <lsikes@calpoly.edu>
2019-12-25 15:34:19 -0800

    Module 7 Upload
    Shadow Mapping

---
+++ module 7 - shadow mapping/shaders/depth-write.fs.glsl
@@ -0,0 +1,9 @@
+// JOHN SEALS AND LACEY SIKES
+precision mediump float;
+
+varying float vDepth;
+
+void main(void) {
+    gl_FragColor = vec4(vDepth, vDepth, vDepth, 1.0);
+    // gl_FragColor = vec4(1.0, vDepth, vDepth, 1.0);
+}
