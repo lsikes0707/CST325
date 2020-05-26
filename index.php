08cef18567f133463afabeed058778d96153bf67
Lacey Gruwell (Sikes) <lsikes@calpoly.edu>
2020-05-26 13:00:27 -0700

    initializing index.php for nav

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+        
+        <style type="text/css">
+            header {
+                text-align: center;
+            }
+            body {
+                background-color: #000000;
+                color: /*#f75216*/#f40480;
+            }
+            
+            /*a {
+                color: #ffffff;
+            }*/
+        
+            #labs, #homework, #finalproject {
+                text-align: center;
+                /*float: none;
+                padding-left: 350px;*/
+            }
+            
+            #footer {
+                text-align: center;
+            }
+        </style>
+        
+    </head>    
+
+
+    <body>
+        <header>
+             <h1><strong>Lacey's</strong> CST-336 Home Page</h1>   
+        </header>
+    
+        <div id="content">
+            <div id="labs">
+                <h2>Lab Assignments</h2>
+                <a href="Labs/Lab1/index.html">Lab 1 </a><br>
+                <a href="Labs/Lab2/777/index.php"> Lab 2</a><br>
+                <a href="Labs/Lab3/index.php">Lab 3</a><br>
+                <a href="Labs/Lab4/scart/index.php">Lab 4</a><br>
+                <a href="Labs/Lab5/index.php">Lab 5</a><br>
+                <a href="Labs/Lab6/login.php">Lab 6</a><br>
+                <a href="Labs/Lab7/hangman/index.html">Lab 7</a><br>
+                <a href="Labs/Lab8/pets/index.php">Lab 8 - Pet Shelter</a><br>
+                <a href="Labs/Lab8/csumb_quiz/index.php">Lab 8 - CSUMB Quiz</a><br>
+                
+            </div>
+        
+            <div id="homework">
+                <h2>Homework Assignments</h2>
+                <a href="HW/HW1/index.html">Homework 1</a><br>
+                <a href="HW/HW2/index.php">Homework 2</a><br>
+                <a href="HW/HW3/index.php">Homework 3</a><br>
+                <a href="HW/HW4/platform.html">Homework 4</a><br>
+            </div>
+            
+            <div id="finalproject">
+                <h2>Final Project</h2>
+                <a href="Final/index.php">SSL Island Mart</a><br><br><br>
+                <!--<a href="https://ssl336final.herokuapp.com/">SSL Island Mart</a><br><br><br> -->
+            </div>
+        </div>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer id="footer">
+            <hr>
+            Internet Programming. 2018&copy; Lacey <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+            It is used for academic purposes only.
+            <figure id="csumbLogo">
+                <img src="img/csumbLogo.jpg" alt="Logo for CSUMB" width="50"/>
+            </figure>
+        </footer>
+        <!-- closing footer -->
+    
+    </body>
+</html>
