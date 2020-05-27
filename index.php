e9c02f37d5cb9fe8d8b34cfa07f1e563a2e4d040
Lacey Gruwell (Sikes) <lsikes@calpoly.edu>
2020-05-27 14:31:48 -0700

    fixing mod4 nav bug

--- index.php
+++ index.php
@@ -45,9 +45,9 @@
                 <a href="module 3/test/test-matrix4.html">Module 3: Matrix3 Testing</a><br>
                 <a href="module 3/test/test-matrix4.html">Module 3: Matrix4 Testing</a><br>
                 <br>
-                <a href="module 4/module 4 - Intro-to-WebGL/Intro-to-WebGL.html">Module 3: Matrix4 Testing</a><br>
+                <a href="module 4 - Intro-to-WebGL/Intro-to-WebGL.html">Module 4: WebGL Demonstration</a><br>
                 <br>
-                <a href="module 5/index.html">Module 3: Matrix4 Testing</a><br>
+                <a href="module 5/index.html">Module 5: Texturing Demonstration</a><br>
                 <br>
                 
             </div>
